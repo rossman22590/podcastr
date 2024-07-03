68fc67a5564b001f3e988d0d5d53c0a551edefab
Ross Cohen <rosscohen1990@gmail.com>
2024-07-03 18:35:35 -0400

    Update auth.config.ts

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 const authConfig = {
   providers: [
     {
-      domain: "https://fond-sawfly-54.clerk.accounts.dev",
+      domain: "https://helpful-tadpole-21.clerk.accounts.dev",
       applicationID: "convex",
     },
   ]
